4306950b28cbc307614f5bf31b90663b31ae912a
gustavtjac <gustavtorup@gmail.com>
2025-02-17 09:26:14 +0100

    init commeit

--- src/main/java/mrrock/com/touristguidedel2/Repository/TouristattractionRepository.java
+++ src/main/java/mrrock/com/touristguidedel2/Repository/TouristattractionRepository.java
@@ -1,8 +1,17 @@
 package mrrock.com.touristguidedel2.Repository;
 
 
+import mrrock.com.touristguidedel2.Model.Touristattraction;
+import org.springframework.boot.autoconfigure.web.reactive.TomcatReactiveWebServerFactoryCustomizer;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class TouristattractionRepository {
+    private List<Touristattraction> touristattractionList = new ArrayList<>();
+
+
+
 }
